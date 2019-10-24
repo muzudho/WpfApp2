1cec3e0d70c8ee04df7490f7ad7fafc37c5a54b7
むずでょ <muzudho1@gmail.com>
2019-10-25 01:59:57 +0900

    絶賛練習中だぜ☆（＾～＾）

---
+++ WpfApp2/AppliesStyle.cs
@@ -0,0 +1,142 @@
+﻿namespace WpfApp2
+{
+    using System.Diagnostics;
+    using System.Windows.Controls;
+    using System.Windows.Documents;
+    using System.Windows.Media;
+
+    public static class AppliesStyle
+    {
+        /// <summary>
+        /// 臙脂色。
+        /// </summary>
+        private static Brush SolidEnjiBrush = new SolidColorBrush(Color.FromRgb(179, 66, 74));
+
+        /// <summary>
+        /// 藍色。
+        /// </summary>
+        private static Brush SolidAiBrush = new SolidColorBrush(Color.FromRgb(35, 71, 148));
+
+        /// <summary>
+        /// 朱色。
+        /// </summary>
+        private static Brush SolidSyuBrush = new SolidColorBrush(Color.FromRgb(239, 69, 74));
+
+        public static void Go(string contents, RichTextBox richTextBox)
+        {
+            FlowDocument flowDoc = new FlowDocument();
+            Paragraph paragraph = new Paragraph();
+
+            // 改行は "\r"、 "\r\n"、 "\n" の３パターンがあるが、どれが入ってるか分からない。
+            // そこで、"\r\n" は "\n" に変換し、残った "\r" を "\n" に変換する。
+            contents = contents.Replace("\r\n", "\n");
+            contents = contents.Replace('\r', '\n');
+            var lines = contents.Split('\n');
+            foreach (var line in lines)
+            {
+                if (line != string.Empty)
+                {
+                    Trace.WriteLine($"Line            | [{line}]");
+                    if (line.StartsWith("$"))
+                    {
+                        var run = new Run(line);
+                        run.Foreground = SolidEnjiBrush;
+                        run.FontSize += 8;
+                        paragraph.Inlines.Add(run);
+                    }
+                    else if (line.StartsWith("#"))
+                    {
+                        var run = new Run(line);
+                        run.Foreground = Brushes.Green;
+                        paragraph.Inlines.Add(run);
+                    }
+                    else if (line.StartsWith("&"))
+                    {
+                        var run = new Run(line);
+                        run.Foreground = SolidAiBrush;
+                        paragraph.Inlines.Add(run);
+                    }
+                    else
+                    {
+                        // { から } までに背景色を付ける。
+                        // ただし、 {{ と }} は無視する。
+
+                        var curr = 0;
+                        while (curr < line.Length)
+                        {
+                            int next;
+
+                            // ひら文
+                            for (; ; )
+                            {
+                                next = line.IndexOf('{', curr);
+                                if (next < 0)
+                                {
+                                    paragraph.Inlines.Add(new Run(line.Substring(curr)));
+                                    goto line_loop;
+                                }
+
+                                if (next + 1 < line.Length && line[next + 1] == '{')
+                                {
+                                    paragraph.Inlines.Add(new Run(line.Substring(curr, next + 2 - curr)));
+                                    curr = next + 2;
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+
+                            // ひら文
+                            var run = new Run(line.Substring(curr, next-curr));
+                            paragraph.Inlines.Add(run);
+
+                            // マーカー
+                            curr = next;
+                            for (; ; )
+                            {
+                                next = line.IndexOf('}', curr);
+                                if (next < 0)
+                                {
+                                    // エラー
+                                    run = new Run(line.Substring(curr));
+                                    run.Background = SolidSyuBrush;
+                                    paragraph.Inlines.Add(run);
+                                    goto line_loop;
+                                }
+
+                                if (next + 1 < line.Length && line[next + 1] == '}')
+                                {
+                                    run = new Run(line.Substring(curr, next + 2 - curr));
+                                    run.Background = Brushes.Yellow;
+                                    paragraph.Inlines.Add(run);
+                                    curr = next + 2;
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+
+                            next += 1;
+                            run = new Run(line.Substring(curr, next-curr));
+                            run.Background = Brushes.Yellow;
+                            paragraph.Inlines.Add(run);
+
+                            // 次のループへ。
+                            curr = next;
+                        }
+                    line_loop:
+                        ;
+                    }
+                }
+
+                // 改行。
+                paragraph.Inlines.Add(new LineBreak());
+            }
+            flowDoc.Blocks.Add(paragraph);
+
+            richTextBox.Document = flowDoc;
+        }
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿namespace WpfApp2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+    using Microsoft.WindowsAPICodePack.Dialogs;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog
+            {
+                Title = "Title",
+                // フォルダ選択ダイアログの場合は true
+                IsFolderPicker = false,
+                // ダイアログが表示されたときの初期ディレクトリを指定
+                InitialDirectory = "適当なパス",
+
+                // ユーザーが最近したアイテムの一覧を表示するかどうか
+                AddToMostRecentlyUsedList = false,
+                // ユーザーがフォルダやライブラリなどのファイルシステム以外の項目を選択できるようにするかどうか
+                AllowNonFileSystemItems = false,
+                // 最近使用されたフォルダが利用不可能な場合にデフォルトとして使用されるフォルダとパスを設定する
+                DefaultDirectory = "適当なパス",
+                // 存在するファイルのみ許可するかどうか
+                EnsureFileExists = true,
+                // 存在するパスのみ許可するかどうか
+                EnsurePathExists = true,
+                // 読み取り専用ファイルを許可するかどうか
+                EnsureReadOnly = false,
+                // 有効なファイル名のみ許可するかどうか（ファイル名を検証するかどうか）
+                EnsureValidNames = true,
+                // 複数選択を許可するかどうか
+                Multiselect = false,
+                // PC やネットワークなどの場所を表示するかどうか
+                ShowPlacesList = true
+            };
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                var contents = File.ReadAllText(dialog.FileName);
+                AppliesStyle.Go(contents, richTextBox);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Create a FlowDocument to contain content for the RichTextBox.
+            FlowDocument myFlowDoc = new FlowDocument();
+            {
+                // Create a paragraph and add the Run and Bold to it.
+                Paragraph myParagraph = new Paragraph();
+                {
+                    // Create a Run of plain text and some bold text.
+                    var myRun = new Run("This is flow content and you can ");
+                    myParagraph.Inlines.Add(myRun);
+
+                    var myBold = new Bold(new Run("edit me!"));
+                    myParagraph.Inlines.Add(myBold);
+
+                    Hyperlink hyperl = new Hyperlink(new Run("Link Text."));
+                    hyperl.NavigateUri = new Uri("http://search.msn.com");
+                    myParagraph.Inlines.Add(hyperl);
+
+                    myParagraph.Inlines.Add(new LineBreak());
+
+                    var myItalic = new Italic(new Run("Italic"));
+                    myParagraph.Inlines.Add(myItalic);
+
+                    var underline = new Underline(new Run("Hello!"));
+                    myParagraph.Inlines.Add(underline);
+
+                    var highlight = new Run("Good morning!");
+                    highlight.Foreground = Brushes.White;
+                    highlight.Background = Brushes.Blue;
+                    myParagraph.Inlines.Add(highlight);
+
+                    var large = new Run("Yes.");
+                    large.FontSize += 8.0;
+                    myParagraph.Inlines.Add(large);
+                }
+
+                // Add the paragraph to the FlowDocument.
+                myFlowDoc.Blocks.Add(myParagraph);
+            }
+
+            // Add initial content to the RichTextBox.
+            richTextBox.Document = myFlowDoc;
+
+            // 全文取得
+            {
+                string plainText = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text;
+                Trace.WriteLine(plainText);
+            }
+
+            // なるべく等幅なフォント指定
+            richTextBox.FontFamily = new FontFamily("ＭＳ ゴシック");
+        }
+    }
+}
